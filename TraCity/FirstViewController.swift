e4734541f75f956f48330c671f3202399e902f0a
Jay Kang <formiscellany@gmail.com>
2018-04-27 23:52:02 -0400

    Initial Commit

---
+++ TraCity/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TraCity
+//
+//  Created by HT on 4/27/18.
+//  Copyright © 2018 JYC Lab. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
